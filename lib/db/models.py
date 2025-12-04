850b059df51c34669f50c84adbda1ed71437ff19
George <gnm7208@gmail.com>
2025-12-04 03:04:27 +0300

    Add models import file for database initialization

---
+++ lib/db/models.py
@@ -0,0 +1,7 @@
+from ..models import Base
+from ..models.user import User
+from ..models.dealership import Dealership
+from ..models.car import Car
+from ..models.sale import Sale
+
+# Import all models to ensure they're registered with Base
